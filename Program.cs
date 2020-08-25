66a5ca52a775db699753a110483e79e9308d67cf
Tom Bloomer <tomwbloomer@gmail.com>
2020-08-25 15:21:46 +0100

    Brackeys C# Challenge 1
    Added the Files

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Brackeys_Csharp_Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Brackeys Story Challenge (Episode 1)";
+            Console.WindowHeight = 60;
+                    
+            Console.WriteLine("Screams could be heard as a ...");
+            
+            Console.ReadLine();
+
+            Console.WriteLine("... walked into a ...");
+
+            Console.ReadLine();
+
+            Console.WriteLine("... to shelter from the rain lashing down outside.\n'You're all in grave danger!' shouted the ...");
+
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+            Console.WriteLine("But... it was too late!\nA Huge ...");
+
+            Console.ReadLine();
+
+            Console.WriteLine("... ripped off the roof, and devoured everyone in one gulp!");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("The End!\nPress any button to Continue");
+                               
+            Console.ReadKey();
+        }
+    }
+}
